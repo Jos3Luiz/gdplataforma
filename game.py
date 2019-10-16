e969f904e4eacaa7e75e84f72d2360547849a8c0
zekkametallica <zekkarock11@gmail.com>
2019-10-15 21:15:30 -0300

    bug

--- game.py
+++ game.py
@@ -1,86 +1,130 @@
+#!/usr/bin/python3
 
 import pygame
-WIDTH=1280
-HEIGHT=720
-
+WIDTH=500
+HEIGHT=480
+IMG_ROOT="img/"
 
 
 class Player:
     def __init__(self,charset=0,lenSprites=8,speed=5):
         self.sprites={}
+        self.lenSprites=lenSprites
         self.LoadSprites()
         self.InitCoord()
         self.speed=speed
         self.isJump=False
-        self.right=False
+        self.up=False
+        self.right=True
         self.walkCount=0
         self.jumpCount=0
         self.stoped=True
         self.facing=1
-    def InitCoord(self,x=10,y=10,width=32,height=32):
+    def InitCoord(self,x=200,y=410,width=32,height=32):
         self.x=x
         self.y=y
         self.widht=width
         self.height=height
-
+        
     def LoadSprites(self):
-        sub=[]
-        for i in range(1,lenSprites+1):
-            sub.append(pygame.image.load("R%i.png"%i))
-        self.sprites["R"]=sub
-        sub=[]
-
-        for i in range(1,lenSprites+1):
-            sub.append(pygame.image.load("L%i.png"%i))
-        self.sprites["L"]=sub
+        self.walkRight=[]
+        for i in range(1,self.lenSprites+1):
+            self.walkRight.append(pygame.image.load("%sR%i.png"%(IMG_ROOT,i)))
     
-    def KeyUpdate(self):
+        self.walkLeft=[]
+        for i in range(1,self.lenSprites+1):
+            self.walkLeft.append(pygame.image.load("%sL%i.png"%(IMG_ROOT,i)))
+    
+    def Update(self):
         keys=pygame.key.get_pressed()
 
-        if keys[pygame.K_SPACE]:
-            if self.left:
+        if keys[pygame.K_LEFT] and self.x > self.speed:
+            self.x-=self.speed
+            self.left = True
+            self.right=False
+        elif keys[pygame.K_RIGHT] and self.x < (WIDTH - self.speed):
+            self.x+=self.speed
+            self.right=True
+            self.left=False
+        else:
+            self.walkCount=0
+        if not(self.isJump):
+            if keys[pygame.K_SPACE]:
+                print("PUAN")
+                self.isJump=True
+                self.up=True
+                self.walkCount=0
+                self.jumpCount=0
+        else:
+            if (self.jumpCount < 10) and self.up:
+                self.y -= (self.jumpCount **2) *0.5 
+                self.jumpCount+=1
+                print (self.jumpCount)
+            if self.jumpCount ==10:
+                self.up=False
+                self.jumpCount=0
+            if self.jumpCount<10 and not(self.up):
+                self.y += ((10-self.jumpCount) **2) *0.5 
+                self.jumpCount=+1
 
+            print(self.y)
+        
 
     def Draw(self,win):
         if self.walkCount>27:
             self.walkCount =0 
         if not (self.stoped):
             if self.left:
-                win.blit(walkLeft[self.walkCount//3],(self.x,self.y))
+                win.blit(self.walkLeft[self.walkCount//3],(self.x,self.y))
                 self.walkCount+=1
             else:
-                win.blit(walkRight[self.walkCount//3],(self.x,self.y))
+                win.blit(self.walkRight[self.walkCount//3],(self.x,self.y))
                 self.walkCount+=1
         else:
             if self.right:
-                win.blit(walkRight[0],(self.x,self.y))
+                win.blit(self.walkRight[0],(self.x,self.y))
             else:
-                win.blit(walkLeft[0],(self.x,self.y))
+                win.blit(self.walkLeft[0],(self.x,self.y))
 
 
 class GameManager:
     def __init__(self):
-        global WIDTH,HEIGHT
+        #global WIDTH,HEIGHT
         pygame.init()
-        self.win=pygame.display.set_mode(WIDHT,HEIGHT)
+        self.win=pygame.display.set_mode((WIDTH,HEIGHT))
     
-        self.bg=pygame.image.load('bg.jpg')
+        self.bg=pygame.image.load(IMG_ROOT+'bg.jpg')
 
-        clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
         self.dinamicItens=[]
-    
 
     def RedrawWindow(self):
         self.win.blit(self.bg,(0,0))
-        for i in self.DinamicItes:
-            i.Draw(win)
+        for i in self.dinamicItens:
+            i.Draw(self.win)
         pygame.display.update()
 
     def AddDinamic(self,dinamicObj):
         self.dinamicItens.append(dinamicObj)
     
 
+    def MainLoop(self):
+        run = True
+        while run:
+            self.clock.tick(27)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
 
+            for obj in self.dinamicItens:
+                obj.Update()
 
+            self.RedrawWindow()
+if __name__=="__main__":
+
+    man = Player()
+    manager= GameManager()
+    manager.AddDinamic(man)
+    manager.MainLoop()
 
